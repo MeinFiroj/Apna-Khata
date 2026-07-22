cd7fa49b872741980c7e3a5c49bfdca931511666
Mein Firoj <firojs2108@gmail.com>
2026-07-22 23:19:53 +0530

    Add jwt token for register

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,18 +1,25 @@
 import bcrypt from 'bcryptjs'
 import validator from 'validator'
 import { adminModel } from '../models/admin.model.js';
+import jwt from 'jsonwebtoken'
 
 export const adminRegController = async (req, res) => {
     const { email, password } = req.body;
     const { existingAdmin } = req;
 
-    if (!validator.isStrongPassword(password, { minLength: 6 }) || !validator.isEmail(email)) return res.status(400).json({ message: "Invalid password" })
+    if (!validator.isStrongPassword(password, { minLength: 6 }) || !validator.isEmail(email)) return res.status(400).json({ message: "Invalid email or password" })
 
     if (existingAdmin) return res.status(422).json({ message: "This email already exist" })
 
     try {
         const passHash = await bcrypt.hash(password, 10);
         const admin = await adminModel.create({ email, password: passHash })
+
+        const token = jwt.sign({ id: admin._id }, process.env.JWT_SECRET_TOKEN, { expiresIn: "7d" })
+        res.cookie("token", token, {
+            httpOnly: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
 
         res.status(201).json({ message: "Admin registered successfully", data: { email: admin.email, id: admin._id } })
     } catch (error) {
