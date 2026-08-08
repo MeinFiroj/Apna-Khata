0b479c19ec7892185a6274ba1e193483aea93cc4
Mein Firoj <firojs2108@gmail.com>
2026-08-08 15:36:24 +0530

    Add Admin-Frontend url in cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express()
 // Middlewares
 app.use(express.json());
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
+    origin : [process.env.FRONTEND_URL, process.env.FRONTEND_URL_ADMIN],
     credentials : true
 }))
 app.use(cookieParser())
