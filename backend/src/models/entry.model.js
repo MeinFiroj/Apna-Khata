de9ba02e17bd44d552eaa725706c0d5bd018fc8b
Mein Firoj <firojs2108@gmail.com>
2026-09-16 21:34:27 +0530

    Update entry model

--- backend/src/models/entry.model.js
+++ backend/src/models/entry.model.js
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 
 const entrySchema = new mongoose.Schema({
     customerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true },
+    currentBalance: { type: Number, ref: "Users", required: true },
+    customerName: { type: String, ref: "Users", required: true },
     type: { type: String, enum: ['credit', 'payment'], required: true },
+    paymentMethod: { type: String, enum: ['cash', 'online'] },
     amount: { type: Number, required: true },
     note: { type: String },
     addedBy: { type: String, enum: ['owner', 'customer'], required: true },
