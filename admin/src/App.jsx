156df202373dcfa6aa85865265cff677dbacc64b
Mein Firoj <firojs2108@gmail.com>
2026-09-16 21:19:48 +0530

    Update eslint.config and App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,40 @@
-import MainRoutes from "./routes/MainRoutes"
-
+import MainRoutes from "./routes/MainRoutes";
+import { useAuth } from "./hooks/useContext";
+import Header from "./components/shared/Header";
+import Loader from "./components/shared/Loader";
+import AuthRoutes from "./routes/AuthRoutes";
+import Navbar from "./components/shared/Navbar";
+import LeftBar from "./components/shared/LeftBar";
 
 const App = () => {
-  return (
-    <div>
-      <MainRoutes/>
-    </div>
-  )
-}
+  const { admin, loading } = useAuth();
 
-export default App
+  if (loading) return <Loader loading={loading} />;
+
+  return (
+    <div
+      className={`md:flex md:items-start md:justify-start w-full h-screen overflow-hidden`}
+    >
+      {admin ? (
+        <>
+          <LeftBar />
+          <div className="w-full h-screen min-h-0 overflow-hidden flex flex-col">
+            <Header />
+            <div className="flex-1 min-h-0">
+              <MainRoutes />
+            </div>
+          </div>
+          <footer className="fixed bottom-0 z-10 px-(--pad-phone) pb-0 pt-2 w-full flex justify-center border-t md:hidden bg-(--clr-bg)">
+            <Navbar />
+          </footer>
+        </>
+      ) : (
+        <div className="w-full h-screen overflow-y-auto">
+          <AuthRoutes />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
