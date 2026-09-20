dc25c8432f87af7b0ef4071d5cb20c34ea10f237
Mein Firoj <firojs2108@gmail.com>
2026-09-20 12:19:44 +0530

    Add History, Profile, Support pages and their routes.

--- frontend/src/routes/AllRoutes.jsx
+++ frontend/src/routes/AllRoutes.jsx
@@ -1,16 +1,23 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "../pages/Home"
-import Login from "../pages/Login"
-import ResetPass from "../pages/ResetPass"
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import ResetPass from "../pages/ResetPass";
+import History from "../pages/History";
+import Support from "../pages/Support";
+import Profile from "../pages/Profile";
 
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path={"/" || "/home"} element={<Home/>} />
-      <Route path={"/login"} element={<Login/>} />
-      <Route path={"/reset-password"} element={<ResetPass/>} />
+      <Route path={"/"} element={<Home />} />
+      <Route path={"/my-passbook"} element={<History />} />
+      <Route path={"/support"} element={<Support />} />
+      <Route path={"/my-profile"} element={<Profile />} />
+      <Route path={"/login"} element={<Login />} />
+      <Route path={"/register"} element={<Login />} />
+      <Route path={"/reset-password"} element={<ResetPass />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default AllRoutes
+export default AllRoutes;
