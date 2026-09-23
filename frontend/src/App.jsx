10dc2b92b8429cf32e15c9f512046bd9d5adaf71
Mein Firoj <firojs2108@gmail.com>
2026-09-23 13:25:42 +0530

    Split Routes into AuthRoutes and MainRoutes, and Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,24 @@
-import Footer from "./components/MainComponents/Footer"
-import Header from "./components/MainComponents/Header"
-import AllRoutes from "./routes/AllRoutes"
+import Footer from "./components/MainComponents/Footer";
+import Header from "./components/MainComponents/Header";
+import MainRoutes from "./routes/MainRoutes";
+import AuthRoutes from "./routes/AuthRoutes";
+import { useAuth } from "./context/auth/useAuth";
 
 const App = () => {
+  const {userData, setUserData} = useAuth()
   return (
-    <div className="h-screen w-screen flex flex-col items-start">
-      <Header/>
-      <AllRoutes/>
-      <Footer/>
+    <div className="min-h-screen w-screen flex flex-col items-start ">
+      {userData ? (
+        <>
+          <Header />
+          <MainRoutes />
+          <Footer />
+        </>
+      ) : (
+        <AuthRoutes />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/routes/MainRoutes.jsx
+++ frontend/src/routes/MainRoutes.jsx
@@ -1,23 +1,19 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
-import Login from "../pages/Login";
-import ResetPass from "../pages/ResetPass";
 import History from "../pages/History";
 import Support from "../pages/Support";
 import Profile from "../pages/Profile";
 
-const AllRoutes = () => {
+const MainRoutes = () => {
   return (
     <Routes>
       <Route path={"/"} element={<Home />} />
       <Route path={"/my-passbook"} element={<History />} />
       <Route path={"/support"} element={<Support />} />
       <Route path={"/my-profile"} element={<Profile />} />
-      <Route path={"/login"} element={<Login />} />
-      <Route path={"/register"} element={<Login />} />
-      <Route path={"/reset-password"} element={<ResetPass />} />
+      <Route path={"*"} element={<Navigate to="/" replace />} />
     </Routes>
   );
 };
 
-export default AllRoutes;
+export default MainRoutes;
