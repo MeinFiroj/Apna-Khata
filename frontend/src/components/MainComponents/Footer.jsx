0cb9c11f84ad0fa6328d1203abca90ee4b835c80
Mein Firoj <firojs2108@gmail.com>
2026-09-23 22:34:46 +0530

    Style update in Header and Footer

--- frontend/src/components/MainComponents/Footer.jsx
+++ frontend/src/components/MainComponents/Footer.jsx
@@ -3,7 +3,7 @@ import Navbar from "../shared/Navbar";
 
 const Footer = () => {
   return (
-    <footer className="upper-shadow p-(--pad-phone) fixed bottom-0 left-0 w-full  md:hidden">
+    <footer className="upper-shadow p-(--pad-phone) fixed bottom-0 left-0 w-full bg-(--clr-bg-off) z-50 md:hidden">
       <nav className="flex items-center justify-around max-w-120 mx-auto">
         <Navbar/>
       </nav>
--- frontend/src/components/MainComponents/Header.jsx
+++ frontend/src/components/MainComponents/Header.jsx
@@ -3,14 +3,14 @@ import Navbar from "../shared/Navbar";
 
 const Header = () => {
   return (
-    <header className="p-(--pad-phone) flex items-center justify-between w-full md:px-(--pad-desk) sticky top-0">
+    <header className="p-(--pad-phone) flex items-center justify-between w-full md:px-(--pad-desk) sticky top-0 bg-(--clr-bg-off) down-shadow z-50">
       <div className="flex items-center gap-2">
         <div className="bg-(--clr-primary) p-2 w-fit rounded-xl">
           <Wallet size={24} color="var(--clr-text-light)" />
         </div>
         <div>
           <h2 className="text-sm">Apna Khata</h2>
-          <h1 className="font-semibold text-(--clr-text-primary) text-lg leading-5">
+          <h1 className="font-semibold text-(--clr-text-primary) text-xl leading-5">
             Dashboard
           </h1>
         </div>
