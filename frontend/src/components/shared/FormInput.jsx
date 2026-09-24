d9eeb9e324e3a4b5622bd1489a15851f4b21c82b
Mein Firoj <firojs2108@gmail.com>
2026-09-24 22:50:28 +0530

    Update FormInput styles

--- frontend/src/components/shared/FormInput.jsx
+++ frontend/src/components/shared/FormInput.jsx
@@ -1,14 +1,18 @@
 import { Eye, EyeOff } from "lucide-react";
 import { useState } from "react";
 
-const FormInput = ({ id, icon: Icon, error, register, type, ...rest }) => {
+const FormInput = ({ id, icon: Icon, error, register, type, inputStyle='default', ...rest }) => {
   const [showPass, setShowPass] = useState(false);
   const isPassword = type === "password";
   const inputType = isPassword && showPass ? "text" : type;
+  const inputStyles = {
+    default : 'border-b-2',
+    box : 'border rounded-lg pl-2 bg-(--clr-input-bg)'
+  }
   return (
     <div className="w-full ">
       {error && <p className="text-red-700 text-sm mb-1">{error.message}</p>}
-      <div className="border-b-2 w-full flex items-center gap-3">
+      <div className={`w-full flex items-center gap-3 ${inputStyles[inputStyle]}`} >
         {Icon && (
           <label htmlFor={id}>
             <Icon size={18} />
