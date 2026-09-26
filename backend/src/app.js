73678a0cb294d667ad7d3e5dadfab55faacf918d
Mein Firoj <firojs2108@gmail.com>
2026-09-26 09:47:25 +0530

    add logging for frontend URL and admin URL in CORS configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,6 +12,8 @@ app.use(cors({
     origin : [process.env.FRONTEND_URL, process.env.FRONTEND_URL_ADMIN , 'http://localhost:5173'],
     credentials : true
 }))
+console.log("RAW FRONTEND_URL:", JSON.stringify(process.env.FRONTEND_URL));
+console.log("RAW FRONTEND_URL_ADMIN:", JSON.stringify(process.env.FRONTEND_URL_ADMIN));
 app.use(cookieParser())
 app.use(express.json());
 
