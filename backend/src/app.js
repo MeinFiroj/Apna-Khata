ab04c2a85fed9c0279e11f1dde70c1660f66bc15
Mein Firoj <firojs2108@gmail.com>
2026-09-26 10:17:40 +0530

    remove debug logging for frontend URLs in CORS configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,8 +12,6 @@ app.use(cors({
     origin : [process.env.FRONTEND_URL, process.env.FRONTEND_URL_ADMIN , 'http://localhost:5173'],
     credentials : true
 }))
-console.log("RAW FRONTEND_URL:", JSON.stringify(process.env.FRONTEND_URL));
-console.log("RAW FRONTEND_URL_ADMIN:", JSON.stringify(process.env.FRONTEND_URL_ADMIN));
 app.use(cookieParser())
 app.use(express.json());
 
